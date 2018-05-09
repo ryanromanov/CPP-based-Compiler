1ae7c6bca07af3557d4c60c802feaf0df0336773
ryanromanov <ryanromanov@github.com>
2018-05-09 00:14:46 -0400

    Initial Commit

---
+++ CPP-based Compiler/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CPP-based Compiler
+//
+//  Created by Ryan Romanosky on 5/9/18.
+//  Copyright © 2018 Ryan Romanosky. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
